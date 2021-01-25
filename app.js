250f75e525e835c4ac0308d79e407b77138fe9cd
aengun <aengun@icloud.com>
2021-01-25 14:38:21 +0900

    module

--- app.js
+++ app.js
@@ -21,15 +21,38 @@
 
 //================================================================
 
+// let fs = require("fs");
+// let http = require("http");
+
+// fs.readFile("./index.html", "utf8", (err, data) => {
+//     console.log(data);
+// }); 
+
+// let server = http.createServer();
+// server.on("request", (req, res)=>{
+//     // console.log("요청이 ...");
+//     res.writeHead(200, {
+//         "Content-Type":"text/html; charset=UTF-8", 
+//     });
+//     res.write("왜왜왜");
+//     res.end();
+// });
+// server.listen(3000);
+
+//================================================================
 let fs = require("fs");
 let http = require("http");
 
-fs.readFile("./js/test.js", "utf8", (err, data) => {
-    console.log(data);
-}); 
+
 
 let server = http.createServer();
-server.on("request", (request, response)=>{
-    console.log("요청이 ...");
+server.on("request", (req, res)=>{
+    fs.readFile("./index.html", "utf8", (err, data) => {
+        res.writeHead(200, {
+            "Content-Type":"text/html; charset=UTF-8", 
+        });
+        res.write(data);
+        res.end();
+    });
 });
 server.listen(3000);
