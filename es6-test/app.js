4eb2094b3692c9ac2d03d654a5afef8c1423fdd0
aengun <aengun@icloud.com>
2021-01-29 12:31:44 +0900

    rendering, mysql설정

---
+++ es6-test/app.js
@@ -0,0 +1,6 @@
+import Index from "./js/index.js";
+
+let index = new Index();
+
+console.log(index.render());
+console.log(__dirname);
---
+++ es6-test/js/index.js
@@ -0,0 +1,7 @@
+export default class Index{
+
+    render(){
+        return "Index Page";
+    }
+
+}
